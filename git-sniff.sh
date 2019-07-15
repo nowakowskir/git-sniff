4c2d404b3da04bd7d3d545e30dd1bd1066ed0857
nowakowskir <nowakowski.r@gmail.com>
2019-07-15 15:00:54 +0200

    Update git-sniff.sh

--- git-sniff.sh
+++ git-sniff.sh
@@ -7,44 +7,49 @@ PARAMS=$(git config --get sniff.params)
 BATCH_MODE=$(git config --get sniff.batchmode)
 
 if ! command -v $SNIFFER > /dev/null; then
-	echo "Sniffer doesn't exist. Use git config --add sniff.cmd <command> to set up code sniffer."
-	
-	exit 1
+        echo "Sniffer doesn't exist. Use git config --add sniff.cmd <command> to set up code sniffer."
+
+        exit 1
 fi
 
 if [ ! -z "$PARAMS" ]; then
-	PARAMS=" $PARAMS"
+        PARAMS=" $PARAMS"
+fi
+
+if [ ${#FILES[@]} -eq 0 ]; then
+        echo -e "\e[0;32mNothing to sniff\e[0m"
+
+        exit 0
 fi
 
 echo -e "\e[0;33mChecking files with code sniffer ($SNIFFER)\e[0m\n"
 
 if [ -z "$BATCH_MODE" ] || [ $BATCH_MODE -ne 1 ]; then
-	for FILE in "${FILES[@]}"
+        for FILE in "${FILES[@]}"
         do
-	        CMD="$SNIFFER$PARAMS $FILE"
-		echo -e "Executing: $CMD"
-		eval $CMD
-		CODE+=$?
+                CMD="$SNIFFER$PARAMS $FILE"
+                echo -e "Executing: $CMD"
+                eval $CMD
+                CODE+=$?
         done
 else
+        for FILE in "${FILES[@]}"
+        do
+                PATHS+=" $BASE_DIR/$FILE"
+        done
 
-	for FILE in "${FILES[@]}"
-	do
-		PATHS+=" $BASE_DIR/$FILE"
-	done
-
-	CMD="$SNIFFER$PARAMS $PATHS"
+        CMD="$SNIFFER$PARAMS $PATHS"
         echo -e "Executing: $CMD"
-	eval $CMD
-	CODE=$?
+        eval $CMD
+        CODE=$?
 fi
 
-if [ $CODE -eq 0 ]; then
+if [ "$CODE" = "0" ]; then
         echo -e "\e[0;32mFiles comply with coding standard\e[0m"
 
-	exit 0
+        exit 0
 else
         echo -e "\e[01;31mFiles don't comply with coding standard\e[0m"
 
-	exit 1
+        exit 1
 fi
